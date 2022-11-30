ef66bdd456243d8ac000ffc0e05460641397f1ea
whistle87 <i379@yandex.ru>
2022-11-30 15:20:48 +0300

    Add emample condition if else

---
+++ Examples_005_ConditioIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Write user name ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Oup, this is MASHA");
+}
+else
+{
+    Console.Write("Hello,");
+    Console.WriteLine(username);
+}
