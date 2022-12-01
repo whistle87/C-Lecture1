9793298aedda024f0b21da4604d258f7597605c7
whistle87 <i379@yandex.ru>
2022-12-01 16:29:23 +0300

    Add finder

---
+++ Example_Finder/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 2, 3, 4, 5, 6, 23, 23 ,45, 34 };
+
+int n = array.Length;
+int find = 23;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
