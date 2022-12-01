981b708c5001dfaecef284867ca1b54a4c781c19
whistle87 <i379@yandex.ru>
2022-12-01 16:28:59 +0300

    Add array library

---
+++ Example_Array_Library/Program.cs
@@ -0,0 +1,45 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int posit = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            posit = index;
+            break;
+        }
+        index++;
+    }
+    return posit;
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+
+int pos = IndexOf(array, 4);
+Console.WriteLine("Pisition "+pos);
